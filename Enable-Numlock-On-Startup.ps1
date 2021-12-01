4bc26347cb62629d27641cb1df24a839d91fabed
vincentwimmer <vincentwimmer@gmail.com>
2021-12-01 14:33:53 -0500

    Update Enable-Numlock-On-Startup.ps1

--- Enable-Numlock-On-Startup.ps1
+++ Enable-Numlock-On-Startup.ps1
@@ -1,2 +1,1 @@
-# Recently Windows has been switching off the Num Lock key on startup by default. This will fix that.
 Set-ItemProperty -Path 'Registry::HKU\.DEFAULT\Control Panel\Keyboard' -Name "InitialKeyboardIndicators" -Value "2"
