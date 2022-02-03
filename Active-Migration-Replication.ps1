f4ce522dd2345a120ee457b1ec1bdeb473e7b8af
vincentwimmer <vincentwimmer@gmail.com>
2022-02-03 13:55:32 -0500

    Create Active-Migration-Replication.ps1

---
+++ Active-Migration-Replication.ps1
@@ -0,0 +1,20 @@
+# This script was created to minimalize system and client downtime and data loss while migrating or separating to a new fileshare when clients have the original fileshare physically mapped.
+# The code will run as long as you need it to and will continuously find recently modified files and push them to the new fileshare as you switch clients over.
+
+$src = 'D:\OldLocat\' # Don't forget the \ after the path!
+$dest = 'Z:\Newcat\' # Don't forget the \ after the path!
+$MinutesSinceModified = 2 # Tune based on src size
+$SecondsTillLoop = 30 # Tune based on update frequency
+
+while ($true) {
+	Get-ChildItem ($src + "*.*")  -rec | Foreach {
+		$lastupdatetime=$_.LastWriteTime
+		$nowtime = get-date
+		if (($nowtime - $lastupdatetime).TotalMinutes -le $MinutesSinceModified){
+			$UpdatePath = $_.FullName.replace($src,$dest)
+			Copy-Item $_.FullName -Destination $UpdatePath
+			Write-Host "Moved Modified File:" $UpdatePath
+		}
+	}
+	Start-Sleep -Seconds $SecondsTillLoop
+}
