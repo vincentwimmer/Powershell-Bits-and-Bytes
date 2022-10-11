a373f6d0ec66cf7582b7e1e7fa308314b909dc41
vincentwimmer <vincentwimmer@gmail.com>
2022-10-11 14:51:19 -0400

    Create Get-Exchange-Distro-Groups-With-Members.ps1

---
+++ Get-Exchange-Distro-Groups-With-Members.ps1
@@ -0,0 +1,20 @@
+$outpath = ([Environment]::GetFolderPath("Desktop")+"\AllDistroGroupsWithMembers.csv")
+$Result=@()
+$groups = Get-DistributionGroup -ResultSize Unlimited
+$totalmbx = $groups.Count
+$i = 1
+$groups | ForEach-Object {
+	Write-Progress -activity "Processing $_.DisplayName" -status "$i out of $totalmbx completed"
+	$group = $_
+	Get-DistributionGroupMember -Identity $group.Name -ResultSize Unlimited | ForEach-Object {
+		$member = $_
+		$Result += New-Object PSObject -property @{
+		GroupName = $group.DisplayName
+		Member = $member.Name
+		EmailAddress = $member.PrimarySMTPAddress
+		RecipientType= $member.RecipientType
+		}
+	}
+	$i++
+}
+$Result | Export-CSV $outpath
